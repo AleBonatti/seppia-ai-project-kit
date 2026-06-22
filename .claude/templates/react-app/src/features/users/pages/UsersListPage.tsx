5a8361b351a797b45dac51e290e309feb42adea2
AleBonatti <la.seppia@gmail.com>
2026-06-22 19:08:04 +0200

    fixing avatar size error

--- .claude/templates/react-app/src/features/users/pages/UsersListPage.tsx
+++ .claude/templates/react-app/src/features/users/pages/UsersListPage.tsx
@@ -117,7 +117,7 @@ export default function UsersListPage() {
                                     <input type="checkbox" className="w-4 h-4 accent-(--accent) cursor-pointer" />
                                 </TableCheckCell>
                                 <TableCell>
-                                    <Avatar initials={user.initials} size={38} />
+                                    <Avatar initials={user.initials} size="md" />
                                 </TableCell>
                                 <TableCell>
                                     <RowTitle title={user.name} />
